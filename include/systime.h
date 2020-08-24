7a6ea0c4745062d74374cbd6268fa0eaae988b4a
DmitryShpakov <work.dmitry.shpakov@gmail.com>
2020-08-24 14:42:38 +0300

    some predefined reload values added

--- include/systime.h
+++ include/systime.h
@@ -8,4 +8,10 @@ typedef uint32_t systime_t;
 systime_t getSysTime(void);
 void initSysTime(uint32_t reloadVal);
 
+#define RELOAD_10MS_64MHZ 	0x09C3FF	//  10 ms @ 64 MHZ CPU clock
+#define RELOAD_1MS_64MHZ 	0x00F9FF	//   1 ms @ 64 MHZ CPU clock
+#define RELOAD_100US_64MHZ	0x0018FF	// 100 us @ 64 MHZ CPU clock
+#define RELOAD_10US_64MHZ	0x00027F	//  10 us @ 64 MHZ CPU clock
+#define RELOAD_1US_64MHZ	0x00003F	//   1 us @ 64 MHZ CPU clock
+
 #endif	// SYS_TIME_H
