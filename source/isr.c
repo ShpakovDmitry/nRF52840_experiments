e6c36fd90bdb59aa85f2aac1ec2854b511a6f000
DmitryShpakov <work.dmitry.shpakov@gmail.com>
2020-09-06 23:16:48 +0300

    added part of IRQ vectors

--- source/isr.c
+++ source/isr.c
@@ -17,6 +17,7 @@ __attribute__((section(".vectors")))
 const void *vectors[] = {
     &stackTop,              /* Initial stack pointer value */
     &cStartup,              /* Reset vector */
+    /* Exception handlers */
     &NmiHandler,            /* NMI */
     &HardFaultHandler,      /* Hard fault*/
     &MemManageFaultHandler, /* Memory management fault */
@@ -30,7 +31,29 @@ const void *vectors[] = {
     0,                      /* Reserved for debug */
     0,                      /* Reserved */
     &PendSVHandler,         /* PendSV */
-    &SysTimeHandler         /* SysTick */
+    &SysTimeHandler,        /* SysTick */
+    /* Interrupt handlers */
+    &PowerClockHandler,     /* ID 0 Power and clock */
+    &RadioHandler,          /* ID 1 Radio */
+    &Uart0Handler,          /* ID 2 Uart0 and Uarte0 */
+    &Spi0Twi0Handler,       /* ID 3 Spi0 and Twi0 */
+    &Spi1Twi1Handler,       /* ID 4 Spi1 and Twi1 */
+    &NfcTagHandler,         /* ID 5 NFC tag */
+    &GpioteHandler,         /* ID 6 GPIO tasks and events */
+    &SaadcHandler,          /* ID 7 Analog to digital converter */
+    &Timer0Handler,         /* ID 8 Timer 0 */
+    &Timer1Handler,         /* ID 9 Timer 1 */
+    &Timer2Handler,         /* ID 10 Timer 2 */
+    &Rtc0Handler,           /* ID 11 Real timer counter 0 */
+    &TempHandler,           /* ID 12 Temperature sensor */
+    &RngHandler,            /* ID 13 Random number generator */
+    &EcbHandler,            /* ID 14 AES electronic code block */
+    &AarCcmHandler,         /* ID 15 AAR and CCM */
+    &WdtHandler,            /* ID 16 Watchdog timer */
+    &Rtc1Handler,           /* ID 17 Real timer counter 1 */
+    &QdecHandler,           /* ID 18 Quadrature decoder */
+    &CompLpcompHandler     /* ID 19 COMP and LPCOMP */
+    //TODO add remaining vectors
 };
 
 __attribute__((interrupt("FIQ"))) void DummyException(void) {
