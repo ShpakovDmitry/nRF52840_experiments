d36d415fc371aa61ef19e9743cd28f0d1a935b8e
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2020-09-18 07:50:07 +0300

    WIP

--- include/clock.h
+++ include/clock.h
@@ -10,20 +10,39 @@ typedef enum {
     HFXO_DEBOUNCE_512US,
     HFXO_DEBOUNCE_1024US,
     HFXO_DEBOUNCE_2048US
-} HfxoDebounceTime;
+} HighFreqDebounceTime;
 
-void startHfxoClock(void);
-void stopHfxoClock(void);
-void setHfxoDebounce(HfxoDebounceTime debounceTime);
+
+typedef enum {
+    CLOCK_HF,
+    CLOCK_LF
+} ClockType;
+
+typedef enum {
+    CLOCK_HIGH_FREQ_INTERNAL,
+    CLOCK_HIGH_FREQ_EXTERNAL,
+    CLOCK_LOW_FREQ_INTERNAL,
+    CLOCK_LOW_FREQ_EXTERNAL,
+} ClockSource;
+
+void Clock_start(ClockType clockType);
+void Clock_stop(ClockType clockType);
+void Clock_setSource(ClockType clockType, ClockSource clockSource);
+void Clock_setDebounce(ClockType clockType, HighFreqDebounceTime highFreqDebounceTime);
+
+
+void Clock_startHighFreqXo(void);
+void Clock_stopHighFreqXo(void);
+void Clock_setDebounce(HfxoDebounceTime debounceTime);
 
 // currently external LFXO is supported
 typedef enum {
     LFCLK_XTAL = 1
 } LfClkSource;
 
-void setLfClkSource(LfClkSource source);
-void startLfxoClock(void);
-void stopLfxoClock(void);
+void Clock_setLfSource(LfClkSource source);
+void Clock_startLfxoClock(void);
+void Clock_stopLfxoClock(void);
 
 
 #endif  // CLOCK_H
