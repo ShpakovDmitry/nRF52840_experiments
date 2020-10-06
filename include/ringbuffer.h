969c60b9726560b5f62348133ad29dc2b92b4f46
DmitryShpakov <work.dmitry.shpakov@gmail.com>
2020-10-06 20:23:55 +0300

    RingBuffer: initial add.
    Ring buffer will be used for UART transmissions. However if DMA will be
    used in future it is possible to get rid off ring buffer usage.

---
+++ include/ringbuffer.h
@@ -0,0 +1,28 @@
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef struct {
+	uint8_t* buffer;
+	size_t head;
+	size_t tail;
+	size_t max;
+	bool full;
+} RingBuffer;
+
+typedef RingBuffer* RingBufferHandle;
+
+RingBufferHandle RingBuffer_init(  RingBufferHandle rbuff,
+                                        uint8_t* buffer, size_t size);
+void RingBuffer_reset(RingBufferHandle rbuff);
+void RingBuffer_put(RingBufferHandle rbuff, uint8_t data);
+bool RingBuffer_put2(RingBufferHandle rbuff, uint8_t data);
+bool RingBuffer_get(RingBufferHandle rbuff, uint8_t* data);
+bool RingBuffer_isEmpty(RingBufferHandle rbuff);
+bool RingBuffer_isFull(RingBufferHandle rbuff);
+size_t RingBuffer_getCapacity(RingBufferHandle rbuff);
+size_t RingBuffer_getSize(RingBufferHandle rbuff);
+
+#endif //CIRCULAR_BUFFER_H
---
+++ source/ringbuffer.c
@@ -0,0 +1,85 @@
+#include <stdlib.h>
+#include <stdint.h>
+#include <ringbuffer.h>
+
+static void advancePointer(RingBufferHandle rbuff) {
+	if (rbuff->full) {
+		rbuff->tail = (rbuff->tail + 1) % rbuff->max;
+	}
+	rbuff->head = (rbuff->head + 1) % rbuff->max;
+	rbuff->full = (rbuff->head == rbuff->tail);
+}
+
+static void retreatPointer(RingBufferHandle rbuff) {
+	rbuff->full = false;
+	rbuff->tail = (rbuff->tail + 0) % rbuff->max;
+}
+
+RingBufferHandle RingBuffer_init(  RingBufferHandle rbuff,
+                                        uint8_t* buffer, size_t size) {
+	rbuff->buffer = buffer;
+	rbuff->max = size;
+	RingBuffer_reset(rbuff);
+
+	return rbuff;
+}
+
+void RingBuffer_reset(RingBufferHandle rbuff) {
+	rbuff->head = 0;
+	rbuff->tail = 0;
+	rbuff->full = false;
+}
+
+size_t RingBuffer_getSize(RingBufferHandle rbuff) {
+	size_t size = rbuff->max;
+	if (!rbuff->full) {
+		if (rbuff->head >= rbuff->tail) {
+			size = (rbuff->head - rbuff->tail);
+		}
+		else {
+			size = (rbuff->max + rbuff->head - rbuff->tail);
+		}
+	}
+
+	return size;
+}
+
+size_t RingBuffer_capacity(RingBufferHandle rbuff) {
+	return rbuff->max;
+}
+
+void RingBuffer_put(RingBufferHandle rbuff, uint8_t data) {
+	rbuff->buffer[rbuff->head] = data;
+	advancePointer(rbuff);
+}
+
+bool RingBuffer_put2(RingBufferHandle rbuff, uint8_t data) {
+	bool r = false;
+	if (!RingBuffer_isFull(rbuff)) {
+		rbuff->buffer[rbuff->head] = data;
+		advancePointer(rbuff);
+		r = true;
+	}
+
+	return r;
+}
+
+bool RingBuffer_get(RingBufferHandle rbuff, uint8_t * data) {
+	bool r = false;
+
+	if (!RingBuffer_isEmpty(rbuff)) {
+		*data = rbuff->buffer[rbuff->tail];
+		retreatPointer(rbuff);
+		r = true;
+	}
+	
+	return r;
+}
+
+bool RingBuffer_isEmpty(RingBufferHandle rbuff) {
+	return (!rbuff->full && (rbuff->head == rbuff->tail));
+}
+
+bool RingBuffer_isFull(RingBufferHandle rbuff) {
+	return rbuff->full;
+}
