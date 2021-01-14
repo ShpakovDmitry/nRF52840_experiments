fcea65da06e925143f371c540eb22166f6db4621
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2021-01-14 10:43:00 +0200

    *)application: moved declarations to .h file

--- source/application/application.c
+++ source/application/application.c
@@ -4,35 +4,26 @@
  *   date: 2020-12-18
  */
 
+#include <stdbool.h>
 #include <util/scheduler/scheduler.h>
 #include <target/nrf52840/nRF52840.h>
 #include "application.h"
 
-static void Application_initHardware();
-static void Application_initScheduler();
-static void Application_runScheduler();
-static void Application_halt();
-static int Application_sendMessage(void);
-static int Application_blinkLed1(void);
-static int Application_blinkLed2(void);
-static int Application_blinkLed3(void);
-static int Application_blinkLed4(void);
 
 void Application_run(void) {
     Application_initHardware();
     Application_initScheduler();
     Application_runScheduler();
-
     // should never get here
     Application_halt();
 }
 
-static void Application_initHardware(void) {
+void Application_initHardware(void) {
     nRF52840_init();
     nRF52840_initLeds();
 }
 
-static void Application_initScheduler(void) {
+void Application_initScheduler(void) {
     const Scheduler_Time SEND_MESSAGE_PERIOD = 1000;
     const Scheduler_Time LED1_BLINK_PERIOD = 500;
     const Scheduler_Time LED2_BLINK_PERIOD = 501;
@@ -46,14 +37,12 @@ static void Application_initScheduler(void) {
     Scheduler_addTask(&Application_blinkLed4,  LED4_BLINK_PERIOD);
 }
 
-static void Application_runScheduler(void) {
+void Application_runScheduler(void) {
     Scheduler_run();
 }
 
-static void Application_halt() {
-    while (1) {
-        ;
-    }
+void Application_halt() {
+    while (true) { ; }
 }
 
 int Application_sendMessage(void) {
@@ -61,22 +50,22 @@ int Application_sendMessage(void) {
     return 0;
 }
 
-static int Application_blinkLed1(void) {
+int Application_blinkLed1(void) {
     nRF52840_invertLed(LED1);
     return 0;
 }
 
-static int Application_blinkLed2(void) {
+int Application_blinkLed2(void) {
     nRF52840_invertLed(LED2);
     return 0;
 }
 
-static int Application_blinkLed3(void) {
+int Application_blinkLed3(void) {
     nRF52840_invertLed(LED3);
     return 0;
 }
 
-static int Application_blinkLed4(void) {
+int Application_blinkLed4(void) {
     nRF52840_invertLed(LED4);
     return 0;
 }
--- source/application/application.h
+++ source/application/application.h
@@ -9,4 +9,14 @@
 
 void Application_run(void);
 
+void Application_initHardware();
+void Application_initScheduler();
+void Application_runScheduler();
+void Application_halt();
+int Application_sendMessage(void);
+int Application_blinkLed1(void);
+int Application_blinkLed2(void);
+int Application_blinkLed3(void);
+int Application_blinkLed4(void);
+
 #endif  // APPLICATION_H
