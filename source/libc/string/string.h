19eb3646f92fcb8d026a7a9340c6193298721da4
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2021-11-16 18:57:31 +0200

    +) cpp portable headerfile

--- source/libc/string/string.h
+++ source/libc/string/string.h
@@ -3,9 +3,17 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 size_t strlen(char* s);
 void reverse(char s[]);
 void itoa(int n, char s[]);
 void* memcpy(void* dst, const void* src, size_t n);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif  // STRING_H
