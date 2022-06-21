42ad29ffb5ff1e6c0a54f4124e4daf5aa3d972cf
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2022-06-21 20:51:54 +0300

    include guards added

---
+++ source/application/arch/nRF52840/drivers/spim/spim.h
@@ -0,0 +1,20 @@
+/*
+ *   file: spim.h
+ * author: ShpakovDmitry
+ *   date: 2022-06-21
+ */
+
+#ifndef SPIM_H
+#define SPIM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif//__cplusplus
+
+
+
+#ifdef __cplusplus
+}
+#endif//__cplusplus
+
+#endif//SPIM_H
