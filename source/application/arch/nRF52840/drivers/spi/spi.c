344f3582154c625de78dd6427c810065b76f2748
ShpakovDmitry <work.dmitry.shpakov@gmail.com>
2022-06-27 21:46:41 +0300

    initial add of spi driver source files

---
+++ source/application/arch/nRF52840/drivers/spi/spi.c
@@ -0,0 +1,9 @@
+/*
+ *   file: spi.c
+ * author: ShpakovDmitry
+ *   date: 2022-06-27
+ */
+
+#include "spi.h"
+
+
---
+++ source/application/arch/nRF52840/drivers/spi/spi.h
@@ -0,0 +1,21 @@
+/*
+ *   file: spi.h
+ * author: ShpakovDmitry
+ *   date: 2022-06-27
+ */
+
+#ifndef SPI_H
+#define SPI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif//__cplusplus
+
+
+
+
+#ifdef __cplusplus
+}
+#endif//__cplusplus
+
+#endif//SPI_H
